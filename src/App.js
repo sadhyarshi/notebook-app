883cad2b23d4790a75632b987139ed9ea96cdb8d
sadhyarshi <sadhyarshii@gmail.com>
2024-07-16 19:30:33 +0530

    Update App.js with new functionality

--- src/App.js
+++ src/App.js
@@ -5,21 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Sample React App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Sample react app React project.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
