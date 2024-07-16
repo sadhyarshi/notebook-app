7a45e427f8a3749c7cb5a1dfe89e2979ff5336c5
sadhyarshi <sadhyarshii@gmail.com>
2024-07-16 19:46:08 +0530

    ADD two number

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleAddition = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setResult(`Sum: ${sum}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Sample React App</h1>
-        <p>
-         Sample react app React project.
-        </p>
-      </header>
+      <h1>Add Two Numbers</h1>
+      <input
+        type="number"
+        placeholder="Enter number 1"
+        value={number1}
+        onChange={(e) => setNumber1(e.target.value)}
+      />
+      <br />
+      <input
+        type="number"
+        placeholder="Enter number 2"
+        value={number2}
+        onChange={(e) => setNumber2(e.target.value)}
+      />
+      <br />
+      <button onClick={handleAddition}>Add Numbers</button>
+      <br />
+      <p>{result}</p>
     </div>
   );
 }
